a51b4f22071db9a5ddc3484309fef3595f4048b7
Henri Remonen <henri@remonen.fi>
2024-09-29 14:18:52 +0300

    test(lexer): move tests to the same function

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,28 +29,18 @@ func TestScanTokens_SingleCharacters(t *testing.T) {
 				{Type: token.EOF, Lexeme: "", Literal: nil, Line: 1},
 			},
 		},
-	}
-
-	for _, tt := range tests {
-		l := New(tt.input)
-		l.scanTokens()
-		if !reflect.DeepEqual(l.tokens, tt.expectedTokens) {
-			t.Errorf("For input %q, expected tokens %v, but got %v", tt.input, tt.expectedTokens, l.tokens)
-		}
-	}
-}
-
-func TestScanTokens_UnexpectedCharacter(t *testing.T) {
-	tests := []struct {
-		input          string
-		expectedTokens []token.Token
-	}{
 		{
 			input: "@#^",
 			expectedTokens: []token.Token{
 				{Type: token.ILLEGAL, Lexeme: "@", Literal: nil, Line: 1},
 				{Type: token.ILLEGAL, Lexeme: "#", Literal: nil, Line: 1},
 				{Type: token.ILLEGAL, Lexeme: "^", Literal: nil, Line: 1},
+				{Type: token.EOF, Lexeme: "", Literal: nil, Line: 1},
+			},
+		},
+		{
+			input: "// This is a comment",
+			expectedTokens: []token.Token{
 				{Type: token.EOF, Lexeme: "", Literal: nil, Line: 1},
 			},
 		},
