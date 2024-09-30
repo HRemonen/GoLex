2b126a7d9da01031de9de2cef0e78394059428f1
Henri Remonen <henri@remonen.fi>
2024-09-30 08:42:15 +0300

    refactor: update lexer code to be more readable and split in to chuncks

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,12 +6,11 @@ tokens that the parser can consume.
 package lexer
 
 import (
-	"fmt"
 	"golox/token"
 	"strconv"
 )
 
-// Lexer is the struct that holds the state of the lexer
+// Lexer holds the state of the lexer
 type Lexer struct {
 	source  string
 	Tokens  []token.Token
@@ -47,7 +46,7 @@ func (l *Lexer) ScanTokens() {
 	})
 }
 
-//nolint:funlen,gocyclo // This function is long and complex because it has to handle all the different token types
+// scanToken processes a single token
 func (l *Lexer) scanToken() {
 	c := l.advance()
 
@@ -75,72 +74,119 @@ func (l *Lexer) scanToken() {
 	case ' ', '\r', '\t':
 		// Ignore whitespace
 	case '\n':
-		// Ignore newline
 		l.line++
 	case '/':
 		if l.match('/') {
-			// A comment goes until the end of the line
-			for l.peek() != '\n' && !l.isAtEnd() {
-				l.advance()
-			}
+			l.lineComment()
 		} else if l.match('*') {
-			// A block comment goes until the end of the block
-			err := l.blockComment()
-			if err != nil {
-				l.addToken(token.ILLEGAL, nil)
-			}
+			l.blockComment()
 		} else {
 			l.addToken(token.SLASH, nil)
 		}
 	case '!':
-		if l.match('=') {
-			l.addToken(token.BANG_EQUAL, nil)
-		} else {
-			l.addToken(token.BANG, nil)
-		}
+		l.addToken(l.matchToken('=', token.BANG_EQUAL, token.BANG), nil)
 	case '=':
-		if l.match('=') {
-			l.addToken(token.EQUAL_EQUAL, nil)
-		} else {
-			l.addToken(token.EQUAL, nil)
-		}
+		l.addToken(l.matchToken('=', token.EQUAL_EQUAL, token.EQUAL), nil)
 	case '<':
-		if l.match('=') {
-			l.addToken(token.LESS_EQUAL, nil)
-		} else {
-			l.addToken(token.LESS, nil)
-		}
+		l.addToken(l.matchToken('=', token.LESS_EQUAL, token.LESS), nil)
 	case '>':
-		if l.match('=') {
-			l.addToken(token.GREATER_EQUAL, nil)
-		} else {
-			l.addToken(token.GREATER, nil)
-		}
-	case '"': // String literals
-		value, err := l.string()
-		if err != nil {
-			l.addToken(token.ILLEGAL, nil)
-		} else {
-			l.addToken(token.STRING, value)
-		}
+		l.addToken(l.matchToken('=', token.GREATER_EQUAL, token.GREATER), nil)
+	case '"':
+		l.processString()
 	default:
 		if l.isDigit(c) {
-			value, err := l.number()
-			if err != nil {
-				l.addToken(token.ILLEGAL, nil)
-			} else {
-				l.addToken(token.NUMBER, value)
-			}
+			l.processNumber()
 		} else if l.isAlpha(c) {
-			value := l.identifier()
-
-			l.addToken(value, nil)
+			l.processIdentifier()
 		} else {
-			l.addToken(token.ILLEGAL, nil)
+			l.addIllegalToken()
 		}
 	}
 }
 
+// Helper for handling strings
+func (l *Lexer) processString() {
+	for l.peek() != '"' && !l.isAtEnd() {
+		if l.peek() == '\n' {
+			l.line++
+		}
+		l.advance()
+	}
+
+	if l.isAtEnd() {
+		l.addIllegalToken()
+		return
+	}
+
+	// Closing quote
+	l.advance()
+	value := l.source[l.start+1 : l.current-1] // Remove quotes
+	l.addToken(token.STRING, value)
+}
+
+// Helper for handling numbers
+func (l *Lexer) processNumber() {
+	for l.isDigit(l.peek()) {
+		l.advance()
+	}
+
+	// Handle fractional part
+	if l.peek() == '.' && l.isDigit(l.peekNext()) {
+		l.advance() // Consume '.'
+		for l.isDigit(l.peek()) {
+			l.advance()
+		}
+	}
+
+	value := l.source[l.start:l.current]
+	parsed, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		l.addIllegalToken()
+	} else {
+		l.addToken(token.NUMBER, parsed)
+	}
+}
+
+// Helper for handling identifiers and keywords
+func (l *Lexer) processIdentifier() {
+	for l.isAlphaNumeric(l.peek()) {
+		l.advance()
+	}
+
+	text := l.source[l.start:l.current]
+	tokenType, ok := token.Keywords[text]
+	if !ok {
+		tokenType = token.IDENTIFIER
+	}
+	l.addToken(tokenType, nil)
+}
+
+// Helper for handling block comments
+func (l *Lexer) blockComment() {
+	for !(l.peek() == '*' && l.peekNext() == '/') && !l.isAtEnd() {
+		if l.peek() == '\n' {
+			l.line++
+		}
+		l.advance()
+	}
+
+	// Consume closing '*/'
+	if !l.isAtEnd() {
+		l.advance() // Consume '*'
+		l.advance() // Consume '/'
+	} else {
+		l.addIllegalToken()
+	}
+}
+
+// Helper for handling single-line comments
+func (l *Lexer) lineComment() {
+	for l.peek() != '\n' && !l.isAtEnd() {
+		l.advance()
+	}
+}
+
+// Adds a token to the list
 func (l *Lexer) addToken(tokenType token.Type, literal interface{}) {
 	text := l.source[l.start:l.current]
 	l.Tokens = append(l.Tokens, token.Token{
@@ -151,129 +197,66 @@ func (l *Lexer) addToken(tokenType token.Type, literal interface{}) {
 	})
 }
 
+// Adds an illegal token
+func (l *Lexer) addIllegalToken() {
+	l.addToken(token.ILLEGAL, nil)
+}
+
+// Advances the lexer to the next character
 func (l *Lexer) advance() rune {
 	l.current++
 	return rune(l.source[l.current-1])
 }
 
-// isAtEnd checks if the lexer has reached the end of the source code
-func (l *Lexer) isAtEnd() bool {
-	return l.current >= len(l.source)
-}
-
-func (l *Lexer) isDigit(c rune) bool {
-	return c >= '0' && c <= '9'
-}
-
-func (l *Lexer) isAlpha(c rune) bool {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
-}
-
-func (l *Lexer) isAlphaNumeric(c rune) bool {
-	return l.isAlpha(c) || l.isDigit(c)
-}
-
-// match checks if the current character matches the expected character
+// Matches the current character with an expected one
 func (l *Lexer) match(expected rune) bool {
-	if l.isAtEnd() {
+	if l.isAtEnd() || rune(l.source[l.current]) != expected {
 		return false
 	}
-
-	if rune(l.source[l.current]) != expected {
-		return false
-	}
-
 	l.current++
 	return true
 }
 
-// peek returns the next character in the source code
+// Matches a token based on a condition
+func (l *Lexer) matchToken(expected rune, matchType, defaultType token.Type) token.Type {
+	if l.match(expected) {
+		return matchType
+	}
+	return defaultType
+}
+
+// Peeks at the next character without advancing
 func (l *Lexer) peek() rune {
 	if l.isAtEnd() {
 		return '\x00'
 	}
-
 	return rune(l.source[l.current])
 }
 
-// peekNext returns the character after the next character in the source code
+// Peeks two characters ahead
 func (l *Lexer) peekNext() rune {
 	if l.current+1 >= len(l.source) {
 		return '\x00'
 	}
-
 	return rune(l.source[l.current+1])
 }
 
-func (l *Lexer) string() (string, error) {
-	for l.peek() != '"' && !l.isAtEnd() {
-		if l.peek() == '\n' {
-			l.line++
-		}
-		l.advance()
-	}
-
-	if l.isAtEnd() {
-		return "", fmt.Errorf("unterminated string")
-	}
-
-	// The closing "
-	l.advance()
-
-	// Trim the surrounding quotes
-	value := l.source[l.start+1 : l.current-1]
-	return value, nil
+// Checks if the end of the source has been reached
+func (l *Lexer) isAtEnd() bool {
+	return l.current >= len(l.source)
 }
 
-func (l *Lexer) number() (float64, error) {
-	for l.isDigit(l.peek()) {
-		l.advance()
-	}
-
-	// Look for a fractional part
-	if l.peek() == '.' && l.isDigit(l.peekNext()) {
-		// Consume the "."
-		l.advance()
-
-		for l.isDigit(l.peek()) {
-			l.advance()
-		}
-	}
-
-	value := l.source[l.start:l.current]
-	return strconv.ParseFloat(value, 64)
+// Checks if the given character is a digit
+func (l *Lexer) isDigit(c rune) bool {
+	return c >= '0' && c <= '9'
 }
 
-func (l *Lexer) identifier() token.Type {
-	for l.isAlphaNumeric(l.peek()) {
-		l.advance()
-	}
-
-	text := l.source[l.start:l.current]
-
-	tokenType, ok := token.Keywords[text]
-	if !ok {
-		tokenType = token.IDENTIFIER
-	}
-
-	return tokenType
+// Checks if the given character is an alphabetical character or an underscore
+func (l *Lexer) isAlpha(c rune) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
 }
 
-func (l *Lexer) blockComment() error {
-	for l.peek() != '*' && l.peekNext() != '/' && !l.isAtEnd() {
-		if l.peek() == '\n' {
-			l.line++
-		}
-		l.advance()
-	}
-
-	// Consume the closing */
-	if l.isAtEnd() {
-		return fmt.Errorf("unterminated block comment")
-	}
-
-	l.advance()
-	l.advance()
-
-	return nil
+// Checks if the character is alphanumeric or an underscore
+func (l *Lexer) isAlphaNumeric(c rune) bool {
+	return l.isAlpha(c) || l.isDigit(c)
 }
